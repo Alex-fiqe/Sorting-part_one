1c4a75fda622e43dbd176c8a4a5c70461f1a581c
Alex-fiqe <elooeliyas955@gmail.com>
2026-03-16 14:53:26 -0700

    Update Bubble-sort.py

--- Bubble-sort.py
+++ Bubble-sort.py
@@ -1,12 +1,12 @@
 def countSwaps(a):
-    swaps = 0
+    m = 0
     n = len(a)
 
     for i in range(n):
         for j in range(n-1):
             if a[j] > a[j+1]:
                 a[j], a[j+1] = a[j+1], a[j]
-                swaps += 1
+                m += 1
 
     print("Array is sorted in", swaps, "swaps.")
     print("First Element:", a[0])
