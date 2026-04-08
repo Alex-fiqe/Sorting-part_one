98c985db18e82ef7b1b68cda734b908e50900431
Alex-fiqe <elooeliyas955@gmail.com>
2026-04-08 15:21:05 -0700

    Update Insertion sort part one.py

--- Insertion sort part one.py
+++ Insertion sort part one.py
@@ -1,17 +1,12 @@
 def insertionSort1(n, arr):
     key = arr[-1]
     i = n - 2
-
     while i >= 0 and arr[i] > key:
         arr[i + 1] = arr[i]
         print(*arr)
         i -= 1
-
     arr[i + 1] = key
     print(*arr)
-
-
-# Input handling
 n = int(input())
 arr = list(map(int, input().split()))
 
